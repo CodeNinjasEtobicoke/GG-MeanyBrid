565bb5ac80c04c625eaa3723ed366b1b009dc877
I Love Iced Coffee <148917437+iam9116@users.noreply.github.com>
2024-02-13 18:06:59 -0500

    code 2
    rip off flappy bird

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour { 
+//Game manager object
+[Header("Game Controller Object for controlling the game")]
+public GameController gameController;
+[Header("default Velocity")]
+public float velocity = 5;
+//Phycics for the bird
+private Rigidbody rb;
+//Height for bird
+private float objectHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //Game controller component
+        //gameController
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
